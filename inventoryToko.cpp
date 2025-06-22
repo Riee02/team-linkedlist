d889cde0ad0fff5bb85f8ae981263393802e455e
Rifqi Fathoni <itsmeriee02@gmail.com>
2025-06-22 14:23:09 +0800

    Membuaat fungsi untuk menambahkan node

--- inventoryToko.cpp
+++ inventoryToko.cpp
@@ -11,4 +11,15 @@ struct toko{
     toko* next;
 };
 
+toko* listBarang(int id, string nama, string kategori, string harga, int stok) {
+    toko* barangBaru=new toko;
+    barangBaru->id=id;
+    barangBaru->nama=nama;
+    barangBaru->kategori=kategori;
+    barangBaru->harga=harga;
+    barangBaru->stok=stok;
+    barangBaru->next=nullptr;
+    return barangBaru;
+}
+
 int main()
